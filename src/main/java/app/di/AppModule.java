45006f36a1bece8cf09cd7b26775c94fdcc54d00
Lily Dayton <lily.dayton.git@gmail.com>
2025-09-06 12:46:46 +0300

    refactor: extract ui package bindings into dedicated module
    - Created ui.Module to encapsulate user interface package bindings
    - Moved all ui bindings from AppModule to ui.Module:
      - Main window components (MainFrame, AppMenuBar, ContentSplitPane, etc.)
      - Display components (AudioFileDisplay, AnnotationDisplay, WordpoolDisplay)
      - Subcomponents (AudioFileList, AnnotationTable, WordpoolList, etc.)
      - Utility services (DialogService, MainWindowAccess)
    - Removed all ui imports from AppModule
    - AppModule now installs ui.Module for cleaner separation

--- src/main/java/app/di/AppModule.java
+++ src/main/java/app/di/AppModule.java
@@ -8,25 +8,6 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import jakarta.inject.Singleton;
 import java.net.http.HttpClient;
-import ui.AppFocusTraversalPolicy;
-import ui.AppMenuBar;
-import ui.ContentSplitPane;
-import ui.ControlPanel;
-import ui.DialogService;
-import ui.DoneButton;
-import ui.MainFrame;
-import ui.MainWindowAccess;
-import ui.WaveformCanvas;
-import ui.WindowLayoutPersistence;
-import ui.annotations.AnnotationDisplay;
-import ui.annotations.AnnotationTable;
-import ui.audiofiles.AudioFileDisplay;
-import ui.audiofiles.AudioFileList;
-import ui.preferences.PreferencesFrame;
-import ui.preferences.PreferencesManager;
-import ui.wordpool.WordpoolDisplay;
-import ui.wordpool.WordpoolList;
-import ui.wordpool.WordpoolTextField;
 
 /**
  * Guice module for dependency injection configuration.
@@ -53,30 +34,12 @@ public class AppModule extends AbstractModule {
         // Install env module for environment and platform bindings
         install(new env.Module());
 
+        // Install ui module for user interface bindings
+        install(new ui.Module());
+
         bind(ActionsFileParser.class);
         bind(ActionsManager.class).in(Singleton.class);
         bind(FmodLibraryLoader.class).in(Singleton.class);
-        bind(PreferencesManager.class).in(Singleton.class);
-        bind(WindowLayoutPersistence.class).in(Singleton.class);
-        bind(MainFrame.class).in(Singleton.class);
-        bind(AppMenuBar.class).in(Singleton.class);
-        bind(ContentSplitPane.class).in(Singleton.class);
-        bind(ControlPanel.class).in(Singleton.class);
-        bind(DoneButton.class).in(Singleton.class);
-        bind(PreferencesFrame.class).in(Singleton.class);
-        bind(WaveformCanvas.class).in(Singleton.class);
-        bind(AudioFileDisplay.class).in(Singleton.class);
-        bind(AnnotationDisplay.class).in(Singleton.class);
-        bind(WordpoolDisplay.class).in(Singleton.class);
-        bind(WordpoolTextField.class).in(Singleton.class);
-        bind(WordpoolList.class).in(Singleton.class);
-        bind(AnnotationTable.class).in(Singleton.class);
-        bind(AudioFileList.class).in(Singleton.class);
-        bind(AppFocusTraversalPolicy.class).in(Singleton.class);
-
-        // Utility services
-        bind(DialogService.class).in(Singleton.class);
-        bind(MainWindowAccess.class).in(Singleton.class);
     }
 
     @Provides
---
+++ src/main/java/ui/Module.java
@@ -0,0 +1,57 @@
+package ui;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import ui.annotations.AnnotationDisplay;
+import ui.annotations.AnnotationTable;
+import ui.audiofiles.AudioFileDisplay;
+import ui.audiofiles.AudioFileList;
+import ui.preferences.PreferencesFrame;
+import ui.preferences.PreferencesManager;
+import ui.wordpool.WordpoolDisplay;
+import ui.wordpool.WordpoolList;
+import ui.wordpool.WordpoolTextField;
+
+/**
+ * Guice module for the ui package.
+ *
+ * <p>Configures bindings for user interface components.
+ */
+public class Module extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        // Main window and frame components
+        bind(MainFrame.class).in(Singleton.class);
+        bind(AppMenuBar.class).in(Singleton.class);
+        bind(ContentSplitPane.class).in(Singleton.class);
+        bind(ControlPanel.class).in(Singleton.class);
+        bind(DoneButton.class).in(Singleton.class);
+        bind(WaveformCanvas.class).in(Singleton.class);
+
+        // Window management and persistence
+        bind(WindowLayoutPersistence.class).in(Singleton.class);
+        bind(AppFocusTraversalPolicy.class).in(Singleton.class);
+
+        // Preferences
+        bind(PreferencesFrame.class).in(Singleton.class);
+        bind(PreferencesManager.class).in(Singleton.class);
+
+        // Audio file display
+        bind(AudioFileDisplay.class).in(Singleton.class);
+        bind(AudioFileList.class).in(Singleton.class);
+
+        // Annotation display
+        bind(AnnotationDisplay.class).in(Singleton.class);
+        bind(AnnotationTable.class).in(Singleton.class);
+
+        // Wordpool display
+        bind(WordpoolDisplay.class).in(Singleton.class);
+        bind(WordpoolList.class).in(Singleton.class);
+        bind(WordpoolTextField.class).in(Singleton.class);
+
+        // Utility services
+        bind(DialogService.class).in(Singleton.class);
+        bind(MainWindowAccess.class).in(Singleton.class);
+    }
+}
