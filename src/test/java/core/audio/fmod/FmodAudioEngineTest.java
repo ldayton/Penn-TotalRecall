3fd792840de8e676985d33f559a69b707f502f25
Lily Dayton <lily.dayton.git@gmail.com>
2025-09-11 18:39:31 +0300

    mark another flaky test

--- src/test/java/core/audio/fmod/FmodAudioEngineTest.java
+++ src/test/java/core/audio/fmod/FmodAudioEngineTest.java
@@ -2,6 +2,7 @@ package core.audio.fmod;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import annotations.AudioHardware;
 import core.audio.AudioHandle;
 import core.audio.AudioMetadata;
 import core.audio.PlaybackHandle;
@@ -193,6 +194,7 @@ class FmodAudioEngineTest {
 
     @Test
     @DisplayName("Should handle complete playback lifecycle")
+    @AudioHardware
     @Timeout(5)
     void testFullPlaybackLifecycle() throws Exception {
         TestListener listener = new TestListener();
